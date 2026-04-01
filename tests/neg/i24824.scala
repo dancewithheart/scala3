1e9d743f1baaa8bcb834c47c4724fbcad994b889
Piotr Paradzinski <dancingwithopenheart@gmail.com>
2026-04-01 10:16:11 +0200

    adjust spelling mistake in 24824 sample
    Signed-off-by: Piotr Paradzinski <dancingwithopenheart@gmail.com>

--- tests/neg/i24824.scala
+++ tests/neg/i24824.scala
@@ -10,5 +10,5 @@ extension [T](s: Seq2[T])
 def f[G](c: Seq2[G]): Unit = ??? // error // Extension without extension methods
 object Crash {
   val s: Seq2[Int] = ???
-  s.sorted.f(Seq2()) // error // mbiguous given instances: both given instance t2 in object TC and given instance t1 in object TC match type TC[Int] of parameter x$1 of method sorted in class Seq2
+  s.sorted.f(Seq2()) // error // ambiguous given instances: both given instance t2 in object TC and given instance t1 in object TC match type TC[Int] of parameter x$1 of method sorted in class Seq2
 }
